c7dee7f6229485c8c537f23119ab8246e7419ffe
Elvis Lin <elvis.125.ma@gmail.com>
2022-04-10 16:12:44 -0700

    populate page with video and playlist queue

--- app.js
+++ app.js
@@ -1,27 +1,110 @@
-var url = "https://cors-anywhere.herokuapp.com/https://ign-apis.herokuapp.com/videos?startIndex=30\u0026count=5";
+var script = document.createElement('script');
+script.src = 'https://ign-apis.herokuapp.com/videos?callback=processVideos'
+document.getElementsByTagName('head')[0].appendChild(script);
 
-var xhr = new XMLHttpRequest();
-xhr.open("GET", url);
+var index = 0;
+var data;
 
+function processVideos(res) {
+   // data = (res.data.splice(0,10))
+   var currVideo = res.data[index];
+   console.log(currVideo)
+   console.log(res.data)
 
-xhr.onreadystatechange = function () {
-   if (xhr.readyState === 4) {
-      console.log(xhr.status);
-      console.log(xhr.responseText);
-   }};
+   /*
+   Video Player
+   */
+   var mainVideo = document.getElementById("playerContainer");
+   var player = document.createElement("video");
+   player.setAttribute('id', "player");
+   player.setAttribute('controls', true);
+   var sourceTag = document.createElement('source');
+   sourceTag.setAttribute('src', currVideo.assets[0].url);
+   sourceTag.setAttribute('type', 'video/mp4');
+   player.appendChild(sourceTag);
+   player.onended = () => {
+      index++;
+      updatePlayer(data[index].assets[0].url);
+   }
+   mainVideo.appendChild(player);
+   /*
+   Title for Video Playing
+   */
+   var mainTitle = document.createElement("h2");
+   mainTitle.innerHTML = currVideo.metadata.title;
+   mainVideo.appendChild(mainTitle);
 
-xhr.send();
+   /*
+   Description for Video Playing
+   */
+   var description = document.createElement("p");
+   description.innerHTML = currVideo.metadata.description;
+   mainVideo.appendChild(description);
 
-// fetch('https://ign-apis.herokuapp.com/videos?startIndex=30\u0026count=5',
-//    {
-//        method: 'GET',
-//        mode: 'no-cors',
-//     //    headers: {
-//     //     'Content-Type': 'application/json',
-//     //     'Content-Type': 'application/x-www-form-urlencoded',
-//     //   },
-//     //   credentials: 'same-origin',
-//    }
-// )
-// // .then(res => res.json())
-// .then(data=>console.log(data))
+   /*
+   Playlist Queue
+   */
+   var nextVideos = res.data.splice(1, 4);
+   var queue = document.getElementById("playlistQueue");
+   nextVideos.forEach(element => {
+      var nextVid = document.createElement('div');
+      nextVid.setAttribute('id', 'nextVid');
+
+      var thumbnail = document.createElement('img');
+      thumbnail.setAttribute('src', element.thumbnails[0].url);
+      nextVid.appendChild(thumbnail);
+
+      var nextVidTitle = document.createElement('h5');
+      nextVidTitle.innerHTML = element.metadata.title;
+      nextVid.append(nextVidTitle);
+
+      queue.appendChild(nextVid);
+      queue.appendChild(document.createElement("hr"));
+      console.log(element);
+   })
+
+   /*
+   Load More Button
+   */
+   const loadMoreButton = document.createElement('button');
+   loadMoreButton.setAttribute('id', 'loadMoreBtn');
+   loadMoreButton.textContent = "Load More";
+   queue.appendChild(loadMoreButton);
+
+   /*
+   Load More Videos in Queue
+   */
+   var nextVideos = res.data.splice(1, 4);
+   var loadMore = document.createElement('div');
+   loadMore.setAttribute('id', 'loadMore');
+   nextVideos.forEach(element => {
+      var nextVid = document.createElement('div');
+      nextVid.setAttribute('id', 'nextVid');
+
+      var thumbnail = document.createElement('img');
+      thumbnail.setAttribute('src', element.thumbnails[0].url);
+      nextVid.appendChild(thumbnail);
+
+      var nextVidTitle = document.createElement('h5');
+      nextVidTitle.innerHTML = element.metadata.title;
+      nextVid.append(nextVidTitle);
+
+      loadMore.appendChild(nextVid);
+      loadMore.appendChild(document.createElement("hr"));
+      queue.appendChild(loadMore);
+   })
+
+   loadMoreButton.onclick = () => {
+      loadMore.style.display = 'initial';
+      loadMoreButton.style.display = 'none';
+   }
+
+}
+
+function updatePlayer(url) {
+   console.log(url)
+   let player = document.getElementById('player');
+   player.setAttribute('src', url);
+   player.load();
+   player.play();
+} 
